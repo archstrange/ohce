9a977c3dfb21c52a87a7597fe504d164dc2f927a
Arch Strange <arch.strange@foxmail.com>
2020-11-08 20:20:08 +0800

    finish 'if' statements

--- src/eval.c
+++ src/eval.c
@@ -16,23 +16,36 @@
 
 #include "eval.h"
 #include "primitive.h"
+#include "idkeyword.h"
 #include <string.h>
 
-static inline bool ohce_output(struct ohce_io *io, Str str)
-{
-	for (size_t i = 0, n = Str_getLength(str); i < n; i++) {
-		if (!io->put(io, Str_get(str, i)))
-		{
-			io->error(io, OHCE_IO_ERR_OFAIL);
-			return false;
-		}
-	}
-	return true;
-}
+static inline bool put_str(struct ohce_io *io, Str str);
+static inline size_t skip_white_spaces(Str source, size_t index);
 
-static bool ohce_eval_dynamics(SadDict sad, struct ohce_io *io, struct ohce_primitive *p)
-{
-}
+enum ExtraTokenType {
+	TT_ERROR = 0,
+	TT_END,
+};
+
+typedef struct SELF {
+	SadDict sad;
+	struct ohce_io *io;
+	struct ohce_primitive *p;
+	size_t index;
+	int tt;
+	Str token;
+} *SELF;
+
+static void next_token(SELF self);
+
+static bool eval_bool(SELF self, bool *res);
+static bool eval_if_block(SELF self, bool alive);
+
+static bool eval_id(SELF self);
+static bool eval_if(SELF self, bool alive);
+static bool eval_for(SELF self, bool alive);
+
+static bool ohce_eval_dynamics(SadDict sad, struct ohce_io *io, struct ohce_primitive *p);
 
 int ohce_eval(SadDict sad, struct ohce_io *io, const char delim1[2], const char delim2[2][2])
 {
@@ -49,7 +62,7 @@ again:
 	ohce_get_primitive(io, &p);
 	switch (p.type) {
 	case OHCE_PRIMITIVE_STATIC:
-		if (ohce_output(io, p.content))
+		if (put_str(io, p.content))
 			goto again;
 		ret = 1;
 		break;
@@ -62,6 +75,200 @@ again:
 
 done:
 	Str_free(p.content);
+	return ret;
+}
+
+static inline bool put_str(struct ohce_io *io, Str str)
+{
+	for (size_t i = 0, n = Str_getLength(str); i < n; i++) {
+		if (!io->put(io, Str_get(str, i))) {
+			io->error(io, OHCE_IO_ERR_OFAIL);
+			return false;
+		}
+	}
+	return true;
+}
+
+static inline size_t skip_white_spaces(Str source, size_t index)
+{
+	size_t n = Str_getLength(source);
+	for (; index < n; index++) {
+		switch (Str_get(source, index)) {
+		default: return index;
+		case ' ':
+		case '\t':
+		case '\v':
+		case '\n':
+		case '\r': break;
+		}
+	}
+	return index;
+}
+
+static void next_token(SELF self)
+{
+	int c;
+	int s = 0;
+	self->tt = TT_END;
+	Str_clear(self->token);
+	size_t n = Str_getLength(self->p->content);
+	self->index = skip_white_spaces(self->p->content, self->index);
+	if (self->index >= n) {
+		return;
+	}
+
+again:
+	if (self->index < n) {
+		c = Str_get(self->p->content, self->index);
+		Str_push(self->token, c);
+	} else {
+		c = 0;
+	}
+
+	s = st_idkeyword(s, c);
+	switch (s) {
+	case 0:
+	case TT_ID:
+	case TT_KW_if: case TT_KW_for:
+	case TT_KW_not: case TT_KW_and: case TT_KW_or:
+	case TT_KW_else: case TT_KW_elif:
+	case TT_KW_endif: case TT_KW_endfor:
+		self->tt = s;
+		return;
+	default:
+		goto again;
+	}
+}
+
+static bool eval_id(SELF self)
+{
+	struct Sad v;
+	if (SadDict_find(self->sad, self->token, &v)) {
+		if (v.type == SAD_STRING) {
+			put_str(self->io, v.str);
+			return true;
+		}
+	}
+	return false;
+}
+
+static bool eval_bool(SELF self, bool *res)
+{
+	struct Sad v;
+	bool ret = true;
+	bool has_not = false;
+	next_token(self);
+	if (self->tt == TT_KW_not) {
+		has_not = true;
+		next_token(self);
+	}
+	if (self->tt != TT_ID)
+		return false;
+	*res = SadDict_find(self->sad, self->token, &v);
+	*res = has_not ? !*res : *res;
+	next_token(self);
+	switch (self->tt) {
+	case TT_END:
+		break;
+	case TT_KW_and:
+		if (res)
+			ret = eval_bool(self, res);
+		else
+			*res = false;
+		break;
+	case TT_KW_or:
+		if (res)
+			*res = true;
+		else
+			ret = eval_bool(self, res);
+		break;
+	default:
+		ret = false;
+		break;
+	}
+	return ret;
+}
+
+static bool eval_if_block(SELF self, bool alive)
+{
+again:
+	ohce_get_primitive(self->io, self->p);
+	if (self->p->type == OHCE_PRIMITIVE_STATIC) {
+		if (alive) put_str(self->io, self->p->content);
+	} else if (self->p->type == OHCE_PRIMITIVE_DYNAMIC) {
+		self->index = 0;
+		next_token(self);
+		switch (self->tt) {
+		case TT_KW_if:
+			eval_if(self, alive);
+			break;
+		case TT_KW_for:
+			eval_for(self, alive);
+			break;
+		default:
+			return true;
+		}
+	}
+	goto again;
+}
+
+static bool eval_if(SELF self, bool alive)
+{
+	bool res;
+	if (!eval_bool(self, &res))
+		return false;
+	if (!eval_if_block(self, alive && res))
+		return false;
+	switch (self->tt) {
+	case TT_KW_else:
+		if (!eval_if_block(self, alive && !res))
+			return false;
+		return self->tt == TT_KW_endif;
+	case TT_KW_elif:
+		return eval_if(self, alive && !res);
+	case TT_KW_endif:
+		return true;
+	default:
+		return false;
+	}
+}
+
+static bool eval_for(SELF self, bool alive)
+{
+
+}
+
+static bool ohce_eval_dynamics(SadDict sad, struct ohce_io *io, struct ohce_primitive *p)
+{
+	size_t n = Str_getLength(p->content);
+	size_t index = skip_white_spaces(p->content, 0);
+	if (index >= n)
+		return true;
+
+	bool ret = true;
+	struct SELF self;
+	self.p = p;
+	self.io = io;
+	self.sad = sad;
+	self.index = index;
+	self.token = Str_new();
+	next_token(&self);
+	switch (self.tt) {
+	case TT_ID:
+		// TODO error check
+		//index = skip_white_spaces(p->content, index);
+		ret =  eval_id(&self);
+		break;
+	case TT_KW_if:
+		ret =  eval_if(&self, true);
+		break;
+	case TT_KW_for:
+		ret =  eval_for(&self, true);
+		break;
+	default:
+		ret = false;
+	}
+	Str_free(self.token);
 	return ret;
 }
 
