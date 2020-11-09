f59f6949d549c617959598e762b1887561001fdf
Arch Strange <arch.strange@foxmail.com>
2020-11-09 21:23:15 +0800

    switch to meson build system

---
+++ src/bin/main.c
@@ -0,0 +1,25 @@
+// ohce -- A simple template engine
+// Copyright (C) 2020  Arch Strange <arch.strange@foxmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "../lib/eval.h"
+
+int main(int argc, const char *argv[])
+{
+	printf("Hello World!\n");
+	return 0;
+}
